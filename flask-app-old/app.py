2c4b93faedba12f4a545b728084158ccfc2cbd44
Ronin Sharma <rrs234@cornell.edu>
2021-12-01 02:16:23 -0500

    updated flask app

---
+++ flask-app-old/app.py
@@ -0,0 +1,83 @@
+from flask import Flask, request, render_template
+import tensorflow as tf
+#from tensorflow import keras
+from tensorflow.keras.layers import (Dense, 
+                                     BatchNormalization, 
+                                     LeakyReLU, 
+                                     Reshape, 
+                                     Conv2DTranspose,
+                                     Conv2D,
+                                     Dropout,
+                                     Flatten)
+#from matplotlib import image
+import matplotlib.pyplot as plt
+
+
+
+
+app = Flask(__name__, static_url_path='')
+
+def make_generator_model():
+    model = tf.keras.Sequential()
+    model.add(Dense(7*7*512, use_bias=False, input_shape=(256,))) #originally 7*7*7, 256*256
+    model.add(BatchNormalization())
+    model.add(LeakyReLU())
+
+    model.add(Reshape((7,7,512)))#was 7,7,512
+#     assert model.output_shape == (None, 7, 7, 512) # Note: None is the batch size
+    model.add(Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False)) #originally 128
+#     assert model.output_shape == (None, 7, 7, 128)
+    model.add(BatchNormalization())
+    model.add(LeakyReLU())
+    
+    model.add(Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False)) #originally 64
+#     assert model.output_shape == (None, 14, 14, 64)
+    model.add(BatchNormalization())
+    model.add(LeakyReLU())
+
+    model.add(Conv2DTranspose(3, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))#originally 3
+#     print(model.output_shape)
+#     assert model.output_shape == (None, 28, 28, 3)
+    return model
+
+@app.route("/")
+def home():
+    return "Hello, World!"
+
+@app.route("/model/") # model?query
+def generateImage():
+    # file = request.args.get("query", default = "test", type = str)
+    # fileOutName = file + "_output.png"
+    fileOutName = 'test.png'
+
+    gen3 = make_generator_model()
+    gen3.load_weights('generator2.h5')
+
+    noise = tf.random.normal([3, 256])*127.5+100
+    #plt.imshow(gen3(noise)[0,:,:,:])
+    #plt.savefig('static/{0}'.format(fileOutName))
+
+    modelOutput = gen3(noise)[0]
+    plt.imsave(fileOutName, modelOutput.numpy().clip(0, 1))
+ 
+    
+    # # Load model
+    # modelPath = ''
+    # model = keras.models.load_model(modelPath)
+
+    # # Generate image and save it in the static folder
+    # output = model(image.imread(file), training=False)
+
+    # fig = plt.figure(figsize=(4,4))
+    # for i in range(output.shape[0]):
+    #     plt.subplot(4, 4, i+1)
+    #     plt.imshow(output[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+    #     plt.axis('off')
+
+    # plt.savefig('static/{0}'.format(fileOutName))
+
+    #return render_template("index.html", fileName = fileOutName)
+    return {'output': 'test'}
+    
+if __name__ == "__main__":
+    app.run()
--- flask-app/app.py
+++ flask-app/app.py
@@ -1,35 +1,66 @@
 from flask import Flask, request, render_template
-from tensorflow import keras
-from matplotlib import image
-from matplotlib import pyplot as plt
+import tensorflow as tf
+#from tensorflow import keras
+from tensorflow.keras.layers import (Dense, 
+                                     BatchNormalization, 
+                                     LeakyReLU, 
+                                     Reshape, 
+                                     Conv2DTranspose,
+                                     Conv2D,
+                                     Dropout,
+                                     Flatten)
+#from matplotlib import image
+import matplotlib.pyplot as plt
+
+
+
 
 app = Flask(__name__, static_url_path='')
 
+def make_generator_model():
+    model = tf.keras.Sequential()
+    model.add(Dense(7*7*512, use_bias=False, input_shape=(256,))) #originally 7*7*7, 256*256
+    model.add(BatchNormalization())
+    model.add(LeakyReLU())
+
+    model.add(Reshape((7,7,512)))#was 7,7,512
+#     assert model.output_shape == (None, 7, 7, 512) # Note: None is the batch size
+    model.add(Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False)) #originally 128
+#     assert model.output_shape == (None, 7, 7, 128)
+    model.add(BatchNormalization())
+    model.add(LeakyReLU())
+    
+    model.add(Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False)) #originally 64
+#     assert model.output_shape == (None, 14, 14, 64)
+    model.add(BatchNormalization())
+    model.add(LeakyReLU())
+
+    model.add(Conv2DTranspose(3, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))#originally 3
+#     print(model.output_shape)
+#     assert model.output_shape == (None, 28, 28, 3)
+    return model
+
 @app.route("/")
 def home():
-    return "Hello, World!"
+    return app.send_static_file('index.html')
 
-@app.route("/model/", methods = ['GET', 'POST'])
+@app.route("/model/") # model?query
 def generateImage():
-    file = request.args.get("query", default = "test", type = str)
-    fileOutName = file + "_output.png"
-    
-    # Load model
-    modelPath = ''
-    model = keras.models.load_model(modelPath)
+    # file = request.args.get("query", default = "test", type = str)
+    # fileOutName = file + "_output.png"
+    fileOutName = 'test.png'
 
-    # Generate image and save it in the static folder
-    output = model(image.imread(file), training=False)
+    gen3 = make_generator_model()
+    gen3.load_weights('generator2.h5')
 
-    fig = plt.figure(figsize=(4,4))
-    for i in range(output.shape[0]):
-        plt.subplot(4, 4, i+1)
-        plt.imshow(output[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
-        plt.axis('off')
+    noise = tf.random.normal([3, 256])*127.5+100
+    #plt.imshow(gen3(noise)[0,:,:,:])
+    #plt.savefig('static/{0}'.format(fileOutName))
 
-    plt.savefig('static/{0}'.format(fileOutName))
-
-    return render_template("index.html", fileName = fileOutName)
+    modelOutput = gen3(noise)[0]
+    plt.imsave(f'static/{fileOutName}', modelOutput.numpy().clip(0, 1))
+ 
+    return {'output': fileOutName}
     
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
